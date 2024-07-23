03c9e4adb50c74380b8c4c41d54d9565f09925a3
hankyung <gksrud246@naver.com>
2024-07-23 17:00:08 +0900

    final

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import mainPhoto from './9.jpg';
+
+function App() {
+  return (
+    <div className="App">
+     <img alt='main' src={mainPhoto} style={{width:'300px',border:'5px inset gray'}}/>
+     <h3>
+      <b>스프링부트+리액트 프로젝트 배포 성공기원!!!</b>
+     </h3>
+    </div>
+  );
+}
+
+export default App;
